88148fa5742e920149e8781cb75b3671231985ac
arnollcvc <leonorcanete0982@gmail.com>
2023-10-27 15:52:57 -0300

    añadiendo algunos comentarios

--- src/socket.js
+++ src/socket.js
@@ -5,15 +5,19 @@ import { v4 as uuid } from "uuid";
 
 io.on("connection", async (socket) => {
   console.log(`a user connected: ${socket.id}`);
-  
-  /* envia el id y el msg */
-  socket.emit("loadPrev", await prevMsgs());
+  /* envia un array con todos los menajes previos */
+  await prevMsgs(socket);
+  /* evento para registrar los mensajes */ 
   socket.on("newMsg", async (msg) => {
+    /* si el contenido del mensaje es mejor o igual a 0 no se ejecuta el resto */
     if (msg.length <= 0) return;
     const id = uuid();
     let data = { id, msg };
+    /* envia el nuevo mensaje a los que esten conectados */
     socket.broadcast.emit("newMsg", data);
+    /* actualiza la lista de mensajes del usuario */
     socket.emit("updateList", data);
+    /* guarda el mensajea recibido */
     await saveMsg(data);
   });
 });
@@ -30,10 +34,11 @@ async function saveMsg({ id, msg }) {
   }
 }
   
-async function prevMsgs() {
+async function prevMsgs(socket) {
   try {
     let data = await client().execute(`SELECT * FROM Message`);
-    return data.rows;
+    if (data.row <= 0) return; 
+    socket.emit("loadPrev", data.row);
   } catch (err) {
     console.error(err.stack);
   }
