939aa982fff5f4c1e44e8959e8a11830a04e322d
arnollcvc <leonorcanete0982@gmail.com>
2023-10-27 15:52:48 -0300

    añadiendo algunos comentarios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import server from "./socket.js";
-import { initDB, getClient as client } from "./db.js";
-import dotenv from "dotenv";
-dotenv.config()
+import { initDB } from "./db.js";
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -9,12 +7,12 @@ const Start = async () => {
   try {
     /* inicializamos la db */
     await initDB();
-
     /* iniciamos el servidor */
     server.listen(PORT, () => console.log(`server listen on port: ${PORT}`));
   } catch (err) {
+    /* registamos el error en consola y detenemos la ejecución */
     console.error("Error al iniciar servidor:", err.stack);
-    // process.exit(1);
+    process.exit(1);
   }
 }
 
